d1a2f0196a019ea0c4add786d7cc6ce6b8ccd0ff
Administrator <nanovia3@naver.com>
2023-02-28 18:09:57 +0900

    1023

--- web-study-ch09/src/main/java/com/saeyan/dao/MemberDAO.java
+++ web-study-ch09/src/main/java/com/saeyan/dao/MemberDAO.java
@@ -25,8 +25,8 @@ public class MemberDAO {
 		Connection conn = null;
 		Context initContext = new InitialContext();
 		Context envContext = (Context) initContext.lookup("java:/comp/env");
-		DataSource dSource = (DataSource) envContext.lookup("jdbc/myoracle");
-		conn = dSource.getConnection();
+		DataSource ds = (DataSource) envContext.lookup("jdbc/myoracle");
+		conn = ds.getConnection();
 		return conn;
 	}
 
@@ -36,6 +36,7 @@ public class MemberDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
+
 		try {
 			conn = getConnection();
 			pstmt = conn.prepareStatement(sql);
@@ -64,6 +65,7 @@ public class MemberDAO {
 				e.printStackTrace();
 			}
 		}
+
 		return result;
 	}
 
@@ -90,7 +92,83 @@ public class MemberDAO {
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return mVo;
+	}
+
+	public int confirmID(String userid) {
+		int result = -1;
+		String sql = "select userid from member where userid=?";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, userid);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				result = 1;
+			} else {
+				result = -1;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
+	}
+
+	public int insertMember(MemberVO mVo) {
+		int result = -1;
+		String sql = "insert into member values(?,?,?,?,?,?)";
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, mVo.getName());
+			pstmt.setString(2, mVo.getUserid());
+			pstmt.setString(3, mVo.getPwd());
+			pstmt.setString(4, mVo.getEmail());
+			pstmt.setString(5, mVo.getPhone());
+			pstmt.setInt(6, mVo.getAdmin());
+			result = pstmt.executeUpdate();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return result;
 	}
 }
